b0bd46816d85ef7bd04404e25320be70e2e0baa4
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-03-17 21:27:07 +0100

    created task model

---
+++ src/models/task.model.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: [true, "A title is required"] },
+  description: { type: String, required: [true, "A description is required"] },
+  completed: { type: Boolean },
+  important: { type: Boolean },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const TaskModel = mongoose.model("Task", TaskSchema);
+
+export default TaskModel;
