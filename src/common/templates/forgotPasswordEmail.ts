6b2ed42531660028a4ba09f3cb8dfa15cee2f8da
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-03-17 16:59:55 +0100

    updated email templates

--- src/common/templates/forgotPasswordEmail.ts
+++ src/common/templates/forgotPasswordEmail.ts
@@ -4,11 +4,7 @@ export const forgotPasswordEmailTemplate = (token: string, name: string) => {
             <h1>Dear ${name}</h1>
             <p>We received a request to reset your password for the Todo App. If you did not make this request, you can safely ignore this email.</p>
 
-            <p>To reset your password, please click the button below:</p>
-
-            <button>
-              <a href="http://localhost:3000/update-password?token=${token}">Reset Account</a>
-            </button>
+            <p>To reset your password, please click the following link: <a href="http://localhost:3000/update-password?token=${token}">Reset Account</a></p>
 
             <p>Note: This link is valid for the next 30 minutes. If you don't verify your account within this timeframe, you may need to request a new verification email.</p>
 
